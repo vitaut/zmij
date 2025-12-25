475a3ba2f62b35d226003bff08e5f15409436617
Alex Guteniev <gutenev@gmail.com>
2025-12-25 17:33:58 +0200

    Support more concurrency in verify program

--- test/verify.cc
+++ test/verify.cc
@@ -6,6 +6,8 @@
 #include <stdio.h>   // printf
 #include <string.h>  // memcpy
 
+#include <bitset>
+#include <limits>
 #include <thread>
 #include <vector>
 
@@ -16,15 +18,17 @@ int main() {
   unsigned concurrency = std::thread::hardware_concurrency();
   constexpr unsigned long long num_floats =
       std::numeric_limits<uint32_t>::max() + 1ULL;
-  uint32_t n = static_cast<uint32_t>(num_floats / concurrency);
-  if (static_cast<uint64_t>(n) * concurrency != num_floats) {
+  if (concurrency == 0 || concurrency > std::numeric_limits<uint32_t>::max()) {
     printf("Unsupported concurrency\n");
     return 1;
   }
 
   std::vector<std::thread> threads(concurrency);
   for (uint32_t i = 0; i < concurrency; ++i) {
-    threads[i] = std::thread([i, n] {
+    uint32_t start = static_cast<uint32_t>(num_floats * i / concurrency);
+    uint32_t end = static_cast<uint32_t>(num_floats * (i + 1) / concurrency);
+    uint32_t n = end - start;
+    threads[i] = std::thread([i, start, n] {
       char actual[zmij::float_buffer_size] = {};
       char expected[32] = {};
       double percent = 100.0 / n;
@@ -32,7 +36,7 @@ int main() {
         if (j % 50'000'000 == 0)
           printf("Thread %d progress: %.2f%%\n", i, j * percent);
 
-        uint32_t bits = i * n + j;
+        uint32_t bits = start + j;
 
         float value = 0;
         memcpy(&value, &bits, sizeof(float));
