c94c79895ea3792a4298d8261bca2f94a533d788
Victor Zverovich <viz@meta.com>
2025-12-27 06:56:42 -0800

    Improve reporting

--- test/verify.cc
+++ test/verify.cc
@@ -23,19 +23,24 @@ int main() {
     return 1;
   }
 
+  std::atomic<uint32_t> num_processed_floats(0);
   std::atomic<bool> has_error(false);
   std::vector<std::thread> threads(concurrency);
   for (uint32_t i = 0; i < concurrency; ++i) {
     uint32_t start = static_cast<uint32_t>(num_floats * i / concurrency);
     uint32_t end = static_cast<uint32_t>(num_floats * (i + 1) / concurrency);
     uint32_t n = end - start;
-    threads[i] = std::thread([i, start, n, &has_error] {
+    threads[i] = std::thread([i, start, n, &num_processed_floats, &has_error] {
       char actual[zmij::float_buffer_size] = {};
       char expected[32] = {};
-      double percent = 100.0 / n;
+      constexpr double percent = 100.0 / num_floats;
       for (uint32_t j = 0; j < n; ++j) {
-        if (j % 50'000'000 == 0)
-          printf("Thread %d progress: %.2f%%\n", i, j * percent);
+        constexpr uint32_t reporting_size = 1 << 21;
+        if (j % reporting_size == 0 && j != 0) {
+          num_processed_floats += reporting_size;
+          if (i == 0 && j % (1 << 23) == 0)
+            printf("Progress: %5.2f%%\n", num_processed_floats * percent);
+        }
 
         uint32_t bits = start + j;
 
