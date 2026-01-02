4168932e2cf4c29a7a310dd2f8826e38e581c08a
Victor Zverovich <viz@meta.com>
2026-01-02 08:49:00 -0800

    Integrate new verification

--- test/double-check.cc
+++ test/double-check.cc
@@ -2,6 +2,8 @@
 // Copyright (c) 2025 - present, Victor Zverovich
 // Distributed under the MIT license (see LICENSE).
 
+#include "double-check.h"
+
 #include <stdint.h>  // uint32_t
 #include <stdio.h>   // printf
 #include <string.h>  // memcpy
@@ -12,7 +14,6 @@
 
 #include "../zmij.cc"
 #include "dragonbox/dragonbox.h"
-#include "double-check.h"
 
 namespace {
 
@@ -82,45 +83,6 @@ auto is_pow10_exact_for_bin_exp(int bin_exp) -> bool {
   return -dec_exp >= exact_begin && -dec_exp <= exact_end;
 }
 
-// Finds all numbers greater or equal to 0xff100000'00000000 in x0 + d * i
-// sequence without enumerating the whole sequence.
-void fast_check(uint64_t x0, uint64_t d, uint64_t count) {
-  uint64_t threshold = 0xff80000000000000;
-  uint128_t mod = uint128_t(1) << 64;
-  uint64_t total_n = 0;
-  int hits_found = 0;
-
-  while (hits_found < 100) {
-    // If x0 is already above threshold, distance to hit is 0.
-    uint64_t n;
-    if (x0 >= threshold) {
-      n = 0;
-    } else {
-      // Target is [threshold - x0, 2^64 - 1 - x0].
-      // This range will never wrap because x0 < threshold.
-      uint128_t lower = threshold - x0;
-      uint128_t upper = uint128_t(0xFFFFFFFFFFFFFFFF) - x0;
-      n = find_min_n(d, mod, lower, upper);
-    }
-
-    if (n == ~uint64_t()) break;
-
-    ++hits_found;
-    total_n += n;
-    uint64_t hit_val = x0 + n * d;
-
-    if (total_n >= count) {
-      printf("Fast check found %lld special cases in %lld values\n", hits_found,
-             total_n);
-      return;
-    }
-
-    // Advance: To find the next hit, we must move at least one step.
-    x0 = hit_val + d;
-    total_n += 1;
-  }
-}
-
 }  // namespace
 
 auto main() -> int {
@@ -137,7 +99,7 @@ auto main() -> int {
     printf("Unsupported exponent\n");
   printf("Verifying binary exponent %d (0x%03x)\n", bin_exp, raw_exp);
 
-  constexpr uint64_t num_significands = uint64_t(1) << 9;  // test a subset
+  constexpr uint64_t num_significands = uint64_t(1) << 32;  // test a subset
 
   constexpr uint64_t exp_bits = uint64_t(raw_exp) << traits::num_sig_bits;
   constexpr int dec_exp = compute_dec_exp(bin_exp, true);
@@ -180,42 +142,83 @@ auto main() -> int {
       constexpr double percent = 100.0 / num_significands;
 
       uint64_t scaled_sig_lo = pow10_lo * (bin_sig_begin << exp_shift);
-      uint64_t scaled_inc = pow10_lo * (1 << exp_shift);
-      fast_check(scaled_sig_lo, scaled_inc, bin_sig_end - bin_sig_begin);
+      uint64_t scaled_step = pow10_lo * (1 << exp_shift);
 
-      for (uint64_t bin_sig = bin_sig_begin; bin_sig < bin_sig_end;
-           ++bin_sig, scaled_sig_lo += scaled_inc) {
-        if ((bin_sig % (1 << 24)) == 0) [[unlikely]] {
-          if (scaled_sig_lo != pow10_lo * (bin_sig << exp_shift)) {
-            printf("Sanity check failed\n");
-            exit(1);
+      // Finds all numbers greater or equal to 1**64 - max_bin_sig_shifted in
+      // start + d * i sequence without enumerating the whole sequence.
+      if (true) {
+        uint64_t start = scaled_sig_lo;
+        uint64_t d = scaled_step;
+        uint64_t count = bin_sig_end - bin_sig_begin;
+        uint64_t threshold = ~uint64_t() - ((bin_sig_end - 1) << exp_shift) + 1;
+        unsigned long long total_n = 0;
+        int hits_found = 0;
+
+        for (;;) {
+          // If start is already above threshold, distance to hit is 0.
+          uint64_t n = 0;
+          if (start < threshold) {
+            // Target is [threshold - start, 2**64 - 1 - start].
+            // This range will never wrap because start < threshold.
+            n = find_min_n(d, uint128_t(1) << 64, threshold - start,
+                           ~uint64_t() - start);
+            if (n == not_found) break;
           }
-          num_processed_doubles += bin_sig - first_unreported;
-          first_unreported = bin_sig;
-          if (i == 0) {
-            auto now = std::chrono::steady_clock::now();
-            if (now - last_update_time >= std::chrono::seconds(1)) {
-              last_update_time = now;
-              printf("Progress: %7.4f%%\n", num_processed_doubles * percent);
+
+          ++hits_found;
+          total_n += n;
+          uint64_t hit_val = start + n * d;
+
+          if (total_n >= count) {
+            printf("Fast check found %d special cases in %lld values\n",
+                   hits_found, total_n);
+            break;
+          }
+          //printf("Found fast: %llx %llx\n", hit_val, bin_sig_begin + total_n);
+          ++num_current_special_cases;
+          uint64_t bin_sig = bin_sig_begin + total_n;
+          uint64_t bits = exp_bits | (bin_sig ^ traits::implicit_bit);
+          if (!verify(bits, bin_sig, bin_exp)) ++num_errors;
+
+          // Advance: To find the next hit, we must move at least one step.
+          start = hit_val + d;
+          total_n += 1;
+        }
+      } else {
+        for (uint64_t bin_sig = bin_sig_begin; bin_sig < bin_sig_end;
+            ++bin_sig, scaled_sig_lo += scaled_step) {
+          if ((bin_sig % (1 << 24)) == 0) [[unlikely]] {
+            if (scaled_sig_lo != pow10_lo * (bin_sig << exp_shift)) {
+              printf("Sanity check failed\n");
+              exit(1);
+            }
+            num_processed_doubles += bin_sig - first_unreported;
+            first_unreported = bin_sig;
+            if (i == 0) {
+              auto now = std::chrono::steady_clock::now();
+              if (now - last_update_time >= std::chrono::seconds(1)) {
+                last_update_time = now;
+                printf("Progress: %7.4f%%\n", num_processed_doubles * percent);
+              }
             }
           }
+
+          // The real power of 10 is in the range [pow10, pow10 + 1) ignoring
+          // the exponent, where pow10 = (pow10_hi << 64) | pow10_lo.
+
+          // Check for possible carry due to pow10 approximation error.
+          // This checks all cases where integral and fractional can be off in
+          // to_decimal. The rest is taken care of by the conservative boundary
+          // checks on the fast path.
+          uint64_t bin_sig_shifted = bin_sig << exp_shift;
+          // scaled_sig_lo = pow10_lo * bin_sig_shifted;
+          bool carry = scaled_sig_lo + bin_sig_shifted < scaled_sig_lo;
+          if (!carry) continue;
+
+          ++num_current_special_cases;
+          uint64_t bits = exp_bits | (bin_sig ^ traits::implicit_bit);
+          if (!verify(bits, bin_sig, bin_exp)) ++num_errors;
         }
-
-        // The real power of 10 is in the range [pow10, pow10 + 1) ignoring
-        // the exponent, where pow10 = (pow10_hi << 64) | pow10_lo.
-
-        // Check for possible carry due to pow10 approximation error.
-        // This checks all cases where integral and fractional can be off in
-        // to_decimal. The rest is taken care of by the conservative boundary
-        // checks on the fast path.
-        uint64_t bin_sig_shifted = bin_sig << exp_shift;
-        // scaled_sig_lo = pow10_lo * bin_sig_shifted;
-        bool carry = scaled_sig_lo + bin_sig_shifted < scaled_sig_lo;
-        if (!carry) continue;
-
-        ++num_current_special_cases;
-        uint64_t bits = exp_bits | (bin_sig ^ traits::implicit_bit);
-        if (!verify(bits, bin_sig, bin_exp)) ++num_errors;
       }
       num_processed_doubles += bin_sig_end - first_unreported;
       num_special_cases += num_current_special_cases;
