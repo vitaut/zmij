70ce98d0e2d769a9196e5756f57a3dc8235aab60
Daniel Green <ddgreen@gmail.com>
2026-01-25 14:54:03 -0500

    Copy the fix from df35b9c for the C version

--- zmij.c
+++ zmij.c
@@ -1202,7 +1202,8 @@ static char* write_significand17(char* buffer, uint64_t value, bool has17digits,
                              vreinterpretq_u16_s8(vdupq_n_s8('0')));
   memcpy(buffer, &str, sizeof(str));
 
-  uint16x8_t is_not_zero = vreinterpretq_u16_u8(vcgtzq_s8(digits));
+  uint16x8_t is_not_zero =
+      vreinterpretq_u16_u8(vcgtzq_s8(vreinterpretq_s8_u8(digits)));
   uint64_t zeroes =
       vget_lane_u64(vreinterpret_u64_u8(vshrn_n_u16(is_not_zero, 4)), 0);
 
