7380f9f0fccfa20678309eca69ef313ca8506833
Victor Zverovich <victor.zverovich@gmail.com>
2026-04-02 12:03:46 -0700

    More verification

--- test/verify.py
+++ test/verify.py
@@ -830,10 +830,10 @@ def find_d2s_edge_case_1(e2, e10, h, p10, p10_exact, SIG_MIN, SIG_MAX):
 
 def find_d2s_edge_case_2(e2, e10, h, p10, p10_exact, SIG_MIN, SIG_MAX):
     # Find: (sig * cache) % den == half_ulp
-    if e10 == 0:
+    if e10 == -1:
         return # special case, no need to check, ulp is 1/2/4/8
 
-    # when e10 > 0, ulp is 2.5, 1.25, 6.25, 3.125, ...
+    # when e10 > -1, ulp is 2.5, 1.25, 6.25, 3.125, ...
     #   sig is multiple of ulp, so last digit + frac == half_ulp never occurs
     known_count = 0
     if e10 < 0:
@@ -842,9 +842,9 @@ def find_d2s_edge_case_2(e2, e10, h, p10, p10_exact, SIG_MIN, SIG_MAX):
         den = 10 ** len(str(ulp))
         known_count = count_mod_fast(ulp, half_ulp, den, SIG_MIN, SIG_MAX)
     
-    TRIM_BITS = 64 + 4
+    TRIM_BITS = FRAC_OFFSET
     NUM =  p10 << (h + 1)
-    DEN =  0xA_0000000000000000_0000000000000000 # 10 << 128
+    DEN =  1 << (128 + EXTRA_SHIFT)
     TOP1 = (((p10 << h) >> TRIM_BITS) << TRIM_BITS)
     TOP2 = (((p10 << h) >> TRIM_BITS) << TRIM_BITS) | ((1 << TRIM_BITS) - 1)
 
@@ -907,7 +907,7 @@ def find_d2s_edge_cases():
     for params in list: find_d2s_edge_case_1(*params)
 
     print("--- Edge case 2 ---")
-    #for params in list: find_d2s_edge_case_2(*params)
+    for params in list: find_d2s_edge_case_2(*params)
     
     print("--- Edge case 3 ---")
     #for params in list: find_d2s_edge_case_3(*params)
